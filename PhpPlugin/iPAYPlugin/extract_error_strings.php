a7803855b542687d0f2f754135c9a84beec92063
poojadev3 <poojagkale1987@gmail.com>
2026-04-27 12:33:26 +0530

    resolving E9001 error

--- PhpPlugin/iPAYPlugin/extract_error_strings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = 'c:/wamp64/www/frontend/php-frontend/php-backend/PhpPlugin/iPAYPlugin/extract_jar/com/fss/plugin/ParseResouce.class';
-$content = file_get_contents($file);
-preg_match_all('/[\x20-\x7E]{5,}/', $content, $matches);
-foreach ($matches[0] as $match) {
-    echo $match . "\n";
-}
